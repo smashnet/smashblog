0beaa61d97224fad4bdf81bcbda815a8db2cfad7
Nicolas Inden <nicolas.inden@innoq.com>
2020-07-16 12:58:51 +0200

    Added Toots! section sourced by my mastodon instance meinungsschubla.de

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat
+$(document).ready(function() {
+    // jQUERY is required!
+    var mapi = new MastodonApi({
+    target_selector  : '#myTimeline'
+    ,instance_uri    : 'https://meinungsschubla.de'
+    ,access_token    : 'dGTAed69jDzKJsmUij_K7CH8BOCZ91_-rUUbq0Lh_fA '
+    ,account_id      : '1'
+    // optional parameters
+    // ===================
+    // - status max
+    //,toots_limit     : 5
+    // - if you are using font-awesome:
+    //,pic_icon        : '<i class="fa fa-picture-o"><\/i>'
+    // or a picture
+    //,pic_icon        : '<img src="mypicicon.gif" />'
+    });
+    });
+    // @license-end
